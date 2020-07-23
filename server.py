5f8a48bf23f3e3b2e014aa9605c472f852ea7a22
MattTheWaffleCat <41089920+MattTheWaffleCat@users.noreply.github.com>
2020-07-22 23:02:03 -0600

    Update server.py

--- server.py
+++ server.py
@@ -7,7 +7,7 @@ app.run(environ.get('PORT'))
 def foo():
     print("searching...")
     #r =requests.get('https://chess-dude-v1.glitch.me/')
-   r =requests.get('https://www.google.com/')
+    r =requests.get('https://www.google.com/')
     print(r.status_code)
     threading.Timer(300, foo).start()
 
