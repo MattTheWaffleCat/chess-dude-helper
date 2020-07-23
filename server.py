a54747e8a8e5c5f255011894a377a821e1c5ebb2
MattTheWaffleCat <41089920+MattTheWaffleCat@users.noreply.github.com>
2020-07-22 23:01:03 -0600

    Update server.py

--- server.py
+++ server.py
@@ -3,3 +3,13 @@ from flask import Flask
 
 app = Flask(__name__)
 app.run(environ.get('PORT'))
+
+def foo():
+    print("searching...")
+    #r =requests.get('https://chess-dude-v1.glitch.me/')
+   r =requests.get('https://www.google.com/')
+    print(r.status_code)
+    threading.Timer(300, foo).start()
+
+print("yea dude, let's rock")
+foo()
