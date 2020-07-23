8ef55d76c08567fc7c0db4202e44ae6e58e99eaa
MattTheWaffleCat <41089920+MattTheWaffleCat@users.noreply.github.com>
2020-07-22 23:00:53 -0600

    Delete bot.py

--- bot.py
+++
@@ -1,9 +0,0 @@
-def foo():
-    print("searching...")
-    #r =requests.get('https://chess-dude-v1.glitch.me/')
-   r =requests.get('https://www.google.com/')
-    print(r.status_code)
-    threading.Timer(300, foo).start()
-
-print("yea dude, let's rock")
-foo()
