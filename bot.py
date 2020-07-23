3d3e6d3e45204e968e1e300fba14b0c9031fb472
MattTheWaffleCat <41089920+MattTheWaffleCat@users.noreply.github.com>
2020-07-22 22:28:23 -0600

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,9 @@
+import requests, time, threading
+
+def foo():
+    print("searching...")
+    r =requests.get('https://chess-dude-v1.glitch.me/')
+    print(r.status_code)
+    threading.Timer(300, foo).start()
+
+foo()
