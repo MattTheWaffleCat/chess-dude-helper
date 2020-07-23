93745b9a2bef58e613487fd270308af19d416227
MattTheWaffleCat <41089920+MattTheWaffleCat@users.noreply.github.com>
2020-07-22 22:40:57 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 def foo():
     print("searching...")
-    r =requests.get('https://chess-dude-v1.glitch.me/')
+    #r =requests.get('https://chess-dude-v1.glitch.me/')
+   r =requests.get('https://www.google.com/')
     print(r.status_code)
     threading.Timer(300, foo).start()
 
