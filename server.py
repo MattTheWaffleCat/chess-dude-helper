2da00b516d0d064c2cf780e4737bfb8f1bb20fc2
MattTheWaffleCat <41089920+MattTheWaffleCat@users.noreply.github.com>
2020-07-22 23:14:17 -0600

    Update server.py

--- server.py
+++ server.py
@@ -1,10 +1,14 @@
-from os import environ
+import os
 from flask import Flask
 
-PORT = 3000
-
 app = Flask(__name__)
-app.run(environ.get(PORT))
+
+@app.route('/')
+
+if __name__ == '__main__':
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
 
 def foo():
     print("searching...")
