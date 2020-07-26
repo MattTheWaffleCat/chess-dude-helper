9cc22b32a8cdcbaa7ef7d5d863df46ba57e5103c
MattTheWaffleCat <41089920+MattTheWaffleCat@users.noreply.github.com>
2020-07-26 12:56:18 -0600

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def index():
 
 def foo():
     print("searching...")
-    r =requests.get('https://chess-dude-v1.glitch.me/')
+    r =requests.get('https://chess-dude-v2.glitch.me/')
     #r =requests.get('https://www.google.com/')
     print(r.status_code)
     threading.Timer(300, foo).start()
